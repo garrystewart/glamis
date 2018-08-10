580f4a0a3db79dd004dbb662eaffc1f8349ff0ed
garrystewart <garry-stewart@hotmail.com>
2018-08-10 19:05:41 +0100

    setup

--- bin/ps/setup.ps1
+++ bin/ps/setup.ps1
@@ -1,9 +1,11 @@
 # install iis
 add-windowsfeature 	Web-Default-Doc, Web-Http-Errors, Web-Static-Content, Web-Stat-Compression, Web-ASP, Web-Includes, Web-Ftp-Service, Web-Mgmt-Service
+
+# configure iis
 Set-WebConfigurationProperty -Filter system.webServer/asp -Name enableParentPaths -Value true
 Set-WebConfigurationProperty -Filter system.webServer/asp -Name scriptErrorSentToBrowser -Value true
 
-# management service
+# setup iis management service
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
 Set-Service -name WMSVC -StartupType Automatic
 Start-service WMSVC
