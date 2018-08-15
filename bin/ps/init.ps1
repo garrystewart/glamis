0fa6c7bc2a2be1ef776d09da48b5fc2420eada19
garrystewart <garry-stewart@hotmail.com>
2018-08-15 19:27:40 +0100

    file cleanup

--- bin/ps/init.ps1
+++ bin/ps/init.ps1
@@ -1,2 +1,3 @@
+# script to run remotely to trigger installation process
 Invoke-WebRequest https://raw.githubusercontent.com/garrystewart/glamis/master/bin/ps/setup.ps1 -UseBasicParsing -OutFile "setup.ps1"
 .\setup.ps1
