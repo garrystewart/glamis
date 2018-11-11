d64c33e58321b9265a71d1659f1eb58f0039886e
garrystewart <garry-stewart@hotmail.com>
2018-11-11 20:15:34 +0000

    display admin password

--- bin/ps/setup.ps1
+++ bin/ps/setup.ps1
@@ -29,3 +29,4 @@ $acl | Set-Acl -Path $path
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing -OutFile "install.ps1"
 .\install.ps1
 choco install Jenkins -y
+Get-Content "C:\Program Files (x86)\jenkins\secrets\initialAdminPassword"
