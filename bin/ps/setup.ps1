d5a9c922911345c2d634f2c675f878d3de1611ee
garrystewart <garry-stewart@hotmail.com>
2018-11-11 17:28:46 +0000

    jenkins install

--- bin/ps/setup.ps1
+++ bin/ps/setup.ps1
@@ -25,3 +25,6 @@ $permission = 'IUSR', 'Modify', 'ContainerInherit, ObjectInherit', 'None', 'Allo
 $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permission
 $acl.SetAccessRule($rule)
 $acl | Set-Acl -Path $path
+
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing -OutFile "install.ps1"
+.\install.ps1
