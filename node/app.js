1b09b72ec4f311da264c9b8108e34ec8d4952739
garrystewart <garry-stewart@hotmail.com>
2018-12-19 17:51:48 +0000

    node

---
+++ node/app.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+const ip = '192.168.0.83';
+const username = '3EDLjI53lNqL14auInL0Xb7xd3Mg6inL4uc7oxTR';
