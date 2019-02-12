40e83c9c4b894d23dd8e759be71a204e303baa5f
garrystewart <garry-stewart@hotmail.com>
2019-02-12 20:45:33 +0000

    push changes

--- wwwroot/hue/node/monitorPlex.js
+++ wwwroot/hue/node/monitorPlex.js
@@ -13,12 +13,13 @@ var $ = jQuery = require('jquery')(window);
 
 var jsonConfig = require('./config.json');
 var jsonPlexCredits = require('./plexCredits.json');
+var intStoredState;
 
 var intPlexSensor;
 
 $.ajax({
     url: `http://${jsonConfig.hue.hostname}/api/${jsonConfig.hue.auth}/sensors`,
-    async: false,
+    async: true,
 }).done(function (response) {
     for (var sensor in response) {
         if (response[sensor].name === 'plex') {
@@ -35,7 +36,7 @@ doPlexPoll();
 function doPlexPoll() {
     $.ajax({
         url: `http://${jsonConfig.plex.host}/status/sessions`,
-        async: false,
+        async: true,
         data: {
             "X-Plex-Token": jsonConfig.plex.auth
         }
@@ -49,7 +50,9 @@ function doPlexPoll() {
                     machineFound = true;
                     switch ($(player).attr('state')) {
                         case 'buffering':
-                            plexSensor(1);
+                            console.log($(player).parents('Video').attr('viewOffset'));
+                            if (intStoredState !== 1) plexSensor(1);
+                            intStoredState = 1;
                             break;
                         case 'playing':
                             var progressMilliseconds = $(player).parents('Video').attr('viewOffset');
@@ -59,13 +62,17 @@ function doPlexPoll() {
                             console.log(progressMilliseconds);
                             console.log(creditsMilliseconds);
                             if (progressMilliseconds < creditsMilliseconds) {
-                                plexSensor(2);
+                                if (intStoredState !== 2) plexSensor(2);
+                                intStoredState = 2;
                             } else {
-                                plexSensor(4);
-                            }                            
+                                if (intStoredState !== 4) plexSensor(4);
+                                intStoredState = 4;
+                            }
                             break;
                         case 'paused':
-                            plexSensor(3);
+                            console.log($(player).parents('Video').attr('viewOffset'));
+                            if (intStoredState !== 3) plexSensor(3);
+                            intStoredState = 3;
                             break;
                         default:
                             console.log(`unknown switch type (${$(player).attr('state')})`);
@@ -76,11 +83,13 @@ function doPlexPoll() {
             });
             if (!machineFound) {
                 console.log(`PLEX - machineIdentifier ${jsonConfig.plex.machineIdentifier} not found`);
-                plexSensor(0);
+                if (intStoredState !== 0) plexSensor(0);
+                intStoredState = 0;
             }
         } else {
             console.log('PLEX - No videos found');
-            plexSensor(0);
+            if (intStoredState !== 0) plexSensor(0);
+            intStoredState = 0;
         }
     }).fail(function (err) {
         console.log(err.statusText);
@@ -92,7 +101,7 @@ function plexSensor(status) {
     $.ajax({
         url: `http://${jsonConfig.hue.hostname}/api/${jsonConfig.hue.auth}/sensors/${intPlexSensor}/state`,
         method: 'PUT',
-        async: false,
+        async: true,
         data: JSON.stringify({
             "status": status
         })
