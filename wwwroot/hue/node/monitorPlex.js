1727fce01bbf85c345a156a2b81a323ae86196c3
garrystewart <garry-stewart@hotmail.com>
2019-02-15 00:00:19 +0000

    timing complete

--- wwwroot/hue/node/monitorPlex.js
+++ wwwroot/hue/node/monitorPlex.js
@@ -11,125 +11,145 @@ const {
 global.document = document;
 var $ = jQuery = require('jquery')(window);
 
+// set up start
+
 var jsonConfig = require('./config.json');
 var jsonPlexCredits = require('./plexCredits.json');
-var intStoredState;
-var blnDefer = false;
+var debugMode = false;
+const delayAdjustment = 1000;
 
-var intPlexSensor;
-
+var plexSensor;
 $.ajax({
     url: `http://${jsonConfig.hue.hostname}/api/${jsonConfig.hue.auth}/sensors`,
     async: false,
 }).done(function (response) {
     for (var sensor in response) {
         if (response[sensor].name === 'plex') {
-            intPlexSensor = sensor;
+            plexSensor = sensor;
             break;
         }
     }
+    main();
 }).fail(function (err) {
     console.log(err.statusText);
 });
 
-doPlexPoll();
+// set up end
 
-function doPlexPoll() {
-    $.ajax({
-        url: `http://${jsonConfig.plex.host}/status/sessions`,
-        async: true,
-        data: {
-            "X-Plex-Token": jsonConfig.plex.auth
+// main code start
+
+function main() {
+
+    var currentPlexSensorStatus;
+    var logsEnabled = false;
+
+    var playing = false;
+    var progressMilliseconds = 0;
+    var anticipatedTime = 0;
+
+    setInterval(function () {
+        if (playing) {
+            if (progressMilliseconds > anticipatedTime) {
+                console.log(' [INFORMATION]  Synchronising anticipated progress with actual progress');
+                anticipatedTime = progressMilliseconds
+            }
+            anticipatedTime += 500;
         }
-    }).done(function (xml) {
-        if ($(xml).find('Video').length) {
-            console.log(`PLEX - Videos found. Checking for machineIdentifier ${jsonConfig.plex.machineIdentifier}...`);
-            var machineFound = false;
-            $(xml).find('Player').each(function (index, player) {
-                if ($(player).attr('machineIdentifier') === jsonConfig.plex.machineIdentifier) {
-                    console.log(`PLEX - machineIdentifier ${jsonConfig.plex.machineIdentifier} found`);
-                    machineFound = true;
-                    switch ($(player).attr('state')) {
-                        case 'buffering':
-                            console.log($(player).parents('Video').attr('viewOffset'));
-                            if (intStoredState !== 1) plexSensor(1);
-                            intStoredState = 1;
-                            break;
-                        case 'playing':
-                            var progressMilliseconds = $(player).parents('Video').attr('viewOffset');
-                            var guid = $(player).parents('Video').attr('guid');
-                            var id = guid.substr(guid.indexOf('tt') + 2, 7);
-                            var creditsMilliseconds = jsonPlexCredits[id].credits;
-                            console.log(progressMilliseconds);
-                            console.log(creditsMilliseconds);
-                            if (progressMilliseconds < creditsMilliseconds) {
-                                if (intStoredState !== 2) {
-                                    blnDefer = true;
-                                    plexSensor(2);
+
+        $.ajax({
+            url: `http://${jsonConfig.plex.host}/status/sessions`,
+            async: true,
+            data: {
+                "X-Plex-Token": jsonConfig.plex.auth
+            }
+        }).done(function (xml) {
+            if ($(xml).find('Video').length) {
+                if (!logsEnabled) console.log(` [INFORMATION]  Looking for machineIdentifier ${jsonConfig.plex.machineIdentifier}...`);
+                var machineFound = false;
+                $(xml).find('Player').each(function (index, player) {
+                    if ($(player).attr('machineIdentifier') === jsonConfig.plex.machineIdentifier) {
+                        if (!logsEnabled) console.log(` [INFORMATION]  machineIdentifier ${jsonConfig.plex.machineIdentifier} found`);
+                        machineFound = true;
+                        var guid = $(player).parents('Video').attr('guid');
+                        var id = guid.substr(guid.indexOf('tt') + 2, 7);
+                        switch ($(player).attr('state')) {
+                            case 'buffering':
+                                if (!logsEnabled) console.log(` [STATUS]  ${jsonPlexCredits[id].title} buffering...`);
+                                changePlexSensorStatus(1);
+                                break;
+                            case 'playing':
+                                progressMilliseconds = Number($(player).parents('Video').attr('viewOffset'));
+                                if (!playing) anticipatedTime = Number(progressMilliseconds);
+                                playing = true;
+                                var creditsMilliseconds = jsonPlexCredits[id].credits;
+                                if (!logsEnabled) console.log(` [STATUS]  ${jsonPlexCredits[id].title} playing...`);
+                                if (anticipatedTime < (creditsMilliseconds - delayAdjustment)) {
+                                    changePlexSensorStatus(2);
+                                } else {
+                                    if (!logsEnabled) console.log(' [INFORMATION]  Credits detected');
+                                    changePlexSensorStatus(4);
                                 }
-                                intStoredState = 2;
-                            } else {
-                                if (intStoredState !== 4) {
-                                    blnDefer = true;
-                                    plexSensor(4);
-                                }
-                                intStoredState = 4;
-                            }
-                            break;
-                        case 'paused':
-                            console.log($(player).parents('Video').attr('viewOffset'));
-                            if (intStoredState !== 3) {
-                                blnDefer = true;
-                                plexSensor(3);
-                            }
-                            intStoredState = 3;
-                            break;
-                        default:
-                            console.log(`unknown switch type (${$(player).attr('state')})`);
+                                break;
+                            case 'paused':
+                                progressMilliseconds = $(player).parents('Video').attr('viewOffset');
+                                //console.log(progressMilliseconds);
+                                if (!logsEnabled) console.log(` [STATUS]  ${jsonPlexCredits[id].title} paused`);
+                                changePlexSensorStatus(3);
+                                break;
+                            default:
+                                console.log('\x1b[33m', '[WARNING]', '\x1b[37m', `unknown switch type (${$(player).attr('state')})`);
+                        }
+                        return false;
                     }
-
-                    return false;
+                });
+                if (!machineFound) {
+                    if (!logsEnabled) console.log(` [INFORMATION]  machineIdentifier ${jsonConfig.plex.machineIdentifier} not found`);
+                    changePlexSensorStatus(0);
                 }
+            } else {
+                if (!logsEnabled) console.log(' [STATUS]  No videos playing');
+                changePlexSensorStatus(0);
+            }
+        }).fail(function (err) {
+            console.log(err.statusText);
+        });
+    }, 500);
+
+    function changePlexSensorStatus(status) {
+        if (status !== 2 && status !== 4) playing = false;
+        if (!debugMode) logsEnabled = true;
+        if (currentPlexSensorStatus !== status) {
+            currentPlexSensorStatus = status;
+            if (!debugMode) logsEnabled = false;
+            console.log('\x1b[33m', '[REQUEST]', '\x1b[37m', `Changing Plex sensor status to ${status}`);
+            $.ajax({
+                url: `http://${jsonConfig.hue.hostname}/api/${jsonConfig.hue.auth}/sensors/${plexSensor}/state`,
+                method: 'PUT',
+                async: true,
+                data: JSON.stringify({
+                    "status": status
+                })
+            }).done(function (response) {
+                if (!checkHueResponseForErrors(response)) console.log('\x1b[32m', '[SUCCESS]', '\x1b[37m', `Plex sensor status changed to ${status}`);
+            }).fail(function (err) {
+                console.log(err.statusText);
             });
-            if (!machineFound) {
-                console.log(`PLEX - machineIdentifier ${jsonConfig.plex.machineIdentifier} not found`);
-                if (intStoredState !== 0) {
-                    blnDefer = true;
-                    plexSensor(0);
-                }
-                intStoredState = 0;
-            }
-        } else {
-            console.log('PLEX - No videos found');
-            if (intStoredState !== 0) {
-                blnDefer = true;
-                plexSensor(0);
-            }
-            intStoredState = 0;
         }
-        if (!blnDefer) doPlexPoll();
-    }).fail(function (err) {
-        console.log(err.statusText);
-        doPlexPoll();
-    });
+    }
 
-}
+    function checkHueResponseForErrors(json) {
+        var errFound = false;
+        $(json).each(function (index, obj) {
+            if ('error' in obj) {
+                arrFound = true;
+                console.log('\x1b[31m', '[ERROR]', '\x1b[37m', `type ${obj.error.type} ${obj.error.address} ${obj.error.description}`);
+            }
+        });
+        if (errFound) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-function plexSensor(status) {
-    $.ajax({
-        url: `http://${jsonConfig.hue.hostname}/api/${jsonConfig.hue.auth}/sensors/${intPlexSensor}/state`,
-        method: 'PUT',
-        async: false,
-        data: JSON.stringify({
-            "status": status
-        })
-    }).done(function (response) {
-        console.log(response);
-        blnDefer = false;
-        doPlexPoll();
-    }).fail(function (err) {
-        console.log(err.statusText);
-        blnDefer = false;
-        doPlexPoll();
-    });
 }
